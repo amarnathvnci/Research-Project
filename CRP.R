5099ce6cabd0668de7ec27ff1847023872021906
amarnathvnci <50916131+amarnathvnci@users.noreply.github.com>
2019-05-28 17:47:42 +0100

    Create CRP.R
    R program for the first portion of my project.

---
+++ CRP.R
@@ -0,0 +1,13 @@
+# Loading the packages ----------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("ggplot2", "tidyverse","dplyr", "corrplot","ggcorrplot","Boruta","caret","e1071","randomForest","ROSE","class")
+
+# Loading The Dataset -----------------------------------------------------
+credit_rating <- read.csv("C:/Users/admin/Desktop/Research Project/credit.csv")
+credit_df<-credit_rating
+View(credit_df)
+
+# Understanding Data ------------------------------------------------------
+head(credit_df)  # headers of the file
+str(credit_df)  # Structure of Data
+sapply(credit_df, FUN=function(x) {sum(is.na(x))}) # Missing Values Check
