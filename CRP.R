458cf2b62c727cff226679843eaaaf308365561f
amarnathvnci <50916131+amarnathvnci@users.noreply.github.com>
2019-06-12 13:50:07 +0100

    Update CRP.R

--- CRP.R
+++ CRP.R
@@ -12,3 +12,39 @@ View(credit_df)
 head(credit_df)  # headers of the file
 str(credit_df)  # Structure of Data
 sapply(credit_df, FUN=function(x) {sum(is.na(x))}) # Missing Values Check
+
+##########Formating Ratings to numeric form ###########
+credit_df$RATING <- as.factor(sapply(credit_df$RATING, FUN = function(x) {if(x == "AAA" | x == "AA+" | x == "AA" | x == "AA-" | 
+                                                                   x == "A+" | x == "A" | x == "A-") {x <- 3} 
+                                                               else if (x == "BBB+" | x == "BBB" | x == "BBB-" | x == "BB+" | x == "BB" | x == "BB-" | 
+ x == "B+" | x == "B" | x == "B-") {x <- 2} else if (x == "CCC+" | x == "CCC" | x == "CC+" | x == "CC" | x == "CC-" | 
+                                                        x == "C+" | x == "C" | x == "C-") {x <- 1}}))
+####################################
+#Removing missing values
+credit_df <- na.omit(credit_df1)
+#######################################
+
+####Correlation among variables
+
+set.seed(7)
+correlationMatrix <- cor(credit_df[,1:123])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+###############################
+
+#########Variable selection based on importance rate
+set.seed(7)
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(RATING~., data=credit_df, method="lvq", preProcess="scale", trControl=control,tuneGrid = data.frame(size = 124, k = 1:123))
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+########################################
