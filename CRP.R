edf32ec1d1fd51d5a00ea7f4e582f814c068eaa0
amarnathvnci <50916131+amarnathvnci@users.noreply.github.com>
2019-07-09 21:10:19 +0100

    Update CRP.R

--- CRP.R
+++ CRP.R
@@ -36,6 +36,16 @@ highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
 print(highlyCorrelated)
 ###############################
 
+##########Formating Ratings to numeric form ###########
+credit_df$RATING <- as.factor(sapply(credit_df$RATING, FUN = function(x) {if(x == "AAA" | x == "AA+" | x == "AA" | x == "AA-" | 
+                                                                   x == "A+" | x == "A" | x == "A-") {x <- 3} 
+                                                               else if (x == "BBB+" | x == "BBB" | x == "BBB-" | x == "BB+" | x == "BB" | x == "BB-" | 
+ x == "B+" | x == "B" | x == "B-") {x <- 2} else if (x == "CCC+" | x == "CCC" | x == "CC+" | x == "CC" | x == "CC-" | 
+                                                        x == "C+" | x == "C" | x == "C-") {x <- 1}}))
+####################################
+
+
+
 #########Variable selection based on importance rate
 set.seed(7)
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
@@ -48,3 +58,191 @@ print(importance)
 # plot importance
 plot(importance)
 ########################################
+
+#######new variable selection
+credit_df <- credit_df[,4:127]
+credit_df$RATING <- droplevels(credit_df$RATING)
+
+
+
+
+# Model Building ----------------------------------------------------------
+set.seed(123)
+my_df1 <- credit_rf
+#my_df1$RATING <- as.factor(my_df1$RATING)
+splitIndex1 <- createDataPartition(my_df1$RATING, p=.10, list=FALSE, times=1)
+splitIndex2 <- createDataPartition(my_df1$RATING, p=.20, list=FALSE, times=1)
+my_df2 <- as.data.frame(my_df1[splitIndex2, ]) 
+train <- my_df1[splitIndex1, ]
+test <- my_df2[-splitIndex1, ]
+
+#SVM model
+svm.model <- svm(RATING ~ ., data = train, kernel = "radial", cost = 1, gamma = 0.1)
+svm.predict <- predict(svm.model, test)
+confusionMatrix(test$RATING, svm.predict)
+
+# Random Forest -----------------------------------------------------------
+set.seed(10)
+rf.model <- randomForest(RATING ~ ., data = train,
+                         ntree = 2000, nodesize = 20)
+
+rf.predict <- predict(rf.model, test)
+confusionMatrix(test$RATING, rf.predict)
+varImpPlot(rf.model)
+
+
+
+
+#Continuous credit rating via Sentiment Analysis
+# Installing packages
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("purr")
+install.packages("psych")
+install.packages("tidytext")
+install.packages("randomcoloR")
+install.packages("broom")
+install.packages("ggrepel")
+install.packages("caret")
+install.packages("e1071")
+install.packages("randomForest")
+install.packages("RColorBrewer")
+# Loading Libraries -------------------------------------------------------
+library(tidyverse)
+library(janitor)
+library(purrr)
+library(psych)
+library(tidytext)
+library(randomcoloR)
+library(tidytext)
+library(broom)
+library(ggrepel)
+library(caret)
+library(e1071)
+library(randomForest)
+library(RColorBrewer)
+
+# Reading File -----------------------------------------------------------
+apple <- read.csv("C:/Users/admin/Desktop/Research Project/apple.csv") %>% clean_names()
+
+
+# Exploring Data ----------------------------------------------------------
+
+glimpse(apple)
+str(apple)
+
+# Checking number of na
+map_df(apple, ~ sum(is.na(.x)))
+
+# All missing value are meaningfully missing values so omitting that
+apple <- apple %>% na.omit()
+
+# Checking distribution + correlation using pairs.panels
+pairs.panels(apple[1:100, 2:5])
+pairs.panels(apple[1:100, 6:11])
+
+
+# Checking the word count from tweets about apple
+apple$wordcount <- sapply(gregexpr("\\S+", apple$text), length)
+
+ggplot(data = apple, aes(x = wordcount, colour = I("black"), fill = I("#099DD9"))) +
+  geom_histogram(binwidth = 3) +
+  labs(x = "Word Count", y = "Frequency", title = "Distribution of word count of text")
+
+# Explotation of Most Common Words
+
+apple$text <- as.character(apple$text)
+apple$sentiment <- as.numeric(apple$sentiment)
+
+apple = apple %>%
+  mutate(grade = ifelse(sentiment > "3","Good",ifelse(sentiment > "2","Average","Bad")))
+#Creating Grade column based on points
+
+apple$grade = as.factor(apple$grade)
+apple$grade = factor(apple$grade,levels(apple$grade)[c(3,1,2)])
+
+#Finding most common words
+apple_Words = apple %>% dplyr::select(text, grade)%>%
+  unnest_tokens(word, text)%>%
+  anti_join(stop_words)%>%
+  dplyr::filter(!str_detect(word, "[0-9]"))
+
+colourCount = 15
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+
+apple_Words %>%
+  count(word, sort = TRUE) %>%
+  top_n(15) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = word)) +
+  geom_col() +
+  labs(title = "Top 15 Significant Words From All Comments", x = "Total Number of Occurances", y = "Word") +
+  coord_flip()+  scale_fill_manual(values = getPalette(colourCount))+
+  theme_bw() + theme(legend.position = "none", plot.title = element_text(hjust = 0.5))
+
+#--------------------------
+
+comment_words = apple_Words %>%
+  count(grade, word, sort = TRUE) %>%
+  ungroup()
+
+total_words = comment_words %>% 
+  group_by(grade) %>% 
+  summarize(total = sum(n))
+
+grade_words = left_join(comment_words, total_words)
+
+grade_words = grade_words %>%
+  bind_tf_idf(word, grade, n)
+
+
+#Words from Good review
+grade_words %>% 
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(grade) %>% 
+  top_n(10) %>% 
+  ungroup %>%
+  dplyr::filter(grade == "Good")%>%
+  ggplot(aes(word, tf_idf)) +
+  geom_col(show.legend = FALSE, fill = 'red') +
+  labs(title ="Top 10 Significant Words from Good posts", x = NULL, y = "tf-idf") +
+  coord_flip()+theme_bw()
+
+#Words from Bad review
+grade_words %>% 
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(grade) %>% 
+  top_n(10) %>% 
+  ungroup %>%
+  dplyr::filter(grade == "Bad")%>%
+  ggplot(aes(word, tf_idf)) +
+  geom_col(show.legend = FALSE, fill = 'Blue') +
+  labs(title ="Top 10 Significant Words from Bad reviews", x = NULL, y = "tf-idf") +
+  coord_flip()
+
+
+
+#Sentiment Analysis
+
+word_final <- inner_join(apple_Words, grade_words[,c("grade","word","tf")], by=c("grade","word"))
+
+word_final <- inner_join(word_final, get_sentiments("afinn"))
+
+sentiment_score <- word_final %>% group_by(grade) %>% summarise(sentiment_score=mean(value)) 
+
+apple_final <- full_join(apple, sentiment_score)
+
+apple_final %>% group_by(grade) %>% summarise(mean(sentiment_score,na.rm = TRUE))
+
+temp <- apple_final %>% dplyr::filter(is.na(sentiment_score)) %>% 
+  mutate(sentiment_score= ifelse(grade=="Good",1.76,ifelse(grade=="Average",1.54,1.13)))
+
+apple_final <- apple_final %>% na.omit()
+
+apple <- unique(rbind(apple_final,temp))
+
+head(apple)
+
